94b85c4b7cfff7e19b51a746c8439959c4ea142a
Alexis Chân Gridel <alexis.gridel@pm.me>
2022-11-07 15:46:32 +0100

    feat(static): set up header page with navbar

--- index.php
+++ index.php
@@ -9,25 +9,11 @@
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
-<h1>Bienvenue sur NetVOD</h1>
-
 <?php
 
+require_once "src/views/header.php";
 require_once 'vendor/autoload.php';
 
-echo "Hello World!"; ?>
-
-<div class="flex justify-between items-center w-full">
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid architecto beatae, dicta dolore dolores et
-        ipsam, iure nesciunt numquam provident quaerat qui saepe tempora, velit veniam vitae voluptate. Doloremque,
-        explicabo.</p>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab delectus ipsam neque possimus, recusandae
-        repellendus tempore tenetur ullam velit vitae? Aliquam cumque eaque eius facere maiores quae quod, soluta
-        voluptatum?</p>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos dolore id inventore libero magnam nesciunt
-        odit officia perferendis quasi, quisquam, veniam vero voluptatibus! Alias, dolore ducimus eum hic necessitatibus
-        sed.</p>
-</div>
-
+?>
 </body>
 </html>
