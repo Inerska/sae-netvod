85a97afb8158439251286ca59468b1965f3eed7e
Alexis Chân Gridel <alexis.gridel@pm.me>
2022-11-07 15:02:48 +0100

    feat(static): add tailwind css framework

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>NetVOD</title>
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
 <h1>Bienvenue sur NetVOD</h1>
@@ -15,5 +16,18 @@
 require_once 'vendor/autoload.php';
 
 echo "Hello World!"; ?>
+
+<div class="flex justify-between items-center w-full">
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid architecto beatae, dicta dolore dolores et
+        ipsam, iure nesciunt numquam provident quaerat qui saepe tempora, velit veniam vitae voluptate. Doloremque,
+        explicabo.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab delectus ipsam neque possimus, recusandae
+        repellendus tempore tenetur ullam velit vitae? Aliquam cumque eaque eius facere maiores quae quod, soluta
+        voluptatum?</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos dolore id inventore libero magnam nesciunt
+        odit officia perferendis quasi, quisquam, veniam vero voluptatibus! Alias, dolore ducimus eum hic necessitatibus
+        sed.</p>
+</div>
+
 </body>
 </html>
