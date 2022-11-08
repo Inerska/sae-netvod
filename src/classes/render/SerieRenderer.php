d94544d45a221999e3fa8f8d763883beb6758c8f
Grossmann Jeremy <jeremy.grossmann4@etu.univ-lorraine.fr>
2022-11-08 11:13:50 +0100

    Revert "modification ajoutée, il reste le dossier images et la vue"
    This reverts commit 1f7f2cc0266357dcc822aa49a2f8002de07aba77.

--- src/classes/render/SerieRenderer.php
+++ src/classes/render/SerieRenderer.php
@@ -17,32 +17,19 @@ class SerieRenderer implements Renderer {
 
     public function render(): string
     {
-        if ($this->serie->id == 0){
-            $html ="<p>La série n'existe pas</p>";
-        } else {
-            $html = "<div class = 'serie' >".
-                "<img src='{$this->serie->image}' alt='image de la série' />".
-                "<h3>Titre : {$this->serie->titre} </h3>";
-            $html .= "<p>Genres : ";
-            foreach ($this->serie->genre as $g) {
-                $html .= $g . ", ";
-            }
-            $html .= "</p>";
-            $html .= "<p>Public visé : ";
-            foreach ($this->serie->publicVise as $p) {
-                $html .= $p . ", ";
-            }
-            $html .= "</p>";
-            $html .= "<p>Descriptif : {$this->serie->descriptif}</p>".
+        $html = "<div class = 'serie' >".
+                "<h3>Titre : {$this->serie->titre} </h3>".
+                "<p>Genre : {$this->serie->genre}</p>".
+                "<p>Public visé : {$this->serie->publicVise}</p>".
+                "<p>Descriptif : {$this->serie->descriptif}</p>".
                 "<p>Année : {$this->serie->annee}</p>".
                 "<p>Date ajout : {$this->serie->dateAjout}</p>".
                 "<p>Nombre d'épisodes : {$this->serie->nbEpisodes}</p>".
                 "<p>Liste des épisodes : </p>".
                 "</div>";
-            foreach ($this->serie->episodes as $episode) {
-                $e = new EpisodeRenderer($episode);
-                $html .= $e->render();
-            }
+        foreach ($this->serie->episodes as $episode) {
+            $e = new EpisodeRenderer($episode);
+            $html .= $e->render();
         }
         return $html;
     }
--- src/classes/video/Serie.php
+++ src/classes/video/Serie.php
@@ -4,21 +4,19 @@ namespace Application\video;
 
 use Application\datalayer\factory\ConnectionFactory;
 use Application\exception\EpisodeNotFoundException;
-use Application\exception\SerieNotFoundException;
-use Application\exception\InvalidPropertyNameException;
 
 
 class Serie {
-    protected int $id = 0;
+    protected int $id;
     protected string $titre;
     protected string $descriptif;
-    protected array $genre = [];
-    protected array $publicVise = [];
+    protected string $genre;
+    protected string $publicVise;
     protected string $image;
     protected string $annee;
     protected string $dateAjout;
-    protected array $episodes = [];
-    protected int $nbEpisodes = 0;
+    protected array $episodes;
+    protected int $nbEpisodes;
 
     public function __construct(int $id){
         ConnectionFactory::setConfig("config.ini");
@@ -27,57 +25,26 @@ class Serie {
         $stmt = $conn->prepare($sql);
         $stmt->execute([$id]);
         $data = $stmt->fetch();
-        if ($data === false){
-            $this->titre = '';
-            $this->descriptif = '';
-            $this->image = '';
-            $this->annee = '';
-            $this->dateAjout = '';
-        } else {
-            //ajout de l'id de la serie
-            $this->id = $data['id'];
-            //ajout du titre de la serie
-            $this->titre = $data['titre'];
-            //ajout du descriptif de la serie
-            $this->descriptif = $data['descriptif'];
-            //ajout de l'image de la serie
-            $this->image = $data['img'];
-            //ajout de l'année de la serie
-            $this->annee = $data['annee'];
-            //ajout de la date d'ajout de la serie
-            $this->dateAjout = $data['date_ajout'];
-            $stmt->closeCursor();
-
-            // Récupération des genres
-            $sql = "select libelle from genre inner join serie_genre on serie_genre.idGenre = genre.idGenre INNER JOIN serie on serie.id = serie_genre.idSerie where serie.id = ?";
-            $stmt = $conn->prepare($sql);
-            $stmt->execute([$id]);
-            while ($data = $stmt->fetch()){
-                $this->genre[] = $data['libelle'];
-            }
-            $stmt->closeCursor();
-
-            // Récupération des publics visés
-            $sql = "select libelle from type inner join serie_type on serie_type.idType = type.idType INNER JOIN serie on serie.id = serie_type.idSerie where serie.id = ?";
-            $stmt = $conn->prepare($sql);
-            $stmt->execute([$id]);
-            while ($data = $stmt->fetch()){
-                $this->publicVise[] = $data['libelle'];
-            }
-            $stmt->closeCursor();
-
-            // Récupération des épisodes
-            $sql = "SELECT * FROM episode WHERE serie_id = ?";
-            $stmt = $conn->prepare($sql);
-            $stmt->execute([$id]);
-            $count = 0;
-            while($data = $stmt->fetch()){
-                $e = new Episode($data['serie_id'],$data['numero']);
-                $this->episodes[] = $e;
-                $count++;
-            }
-            $this->nbEpisodes = $count;
+        $this->id = $data['id'];
+        $this->titre = $data['titre'];
+        $this->descriptif = $data['descriptif'];
+        $this->image = $data['img'];
+        $this->annee = $data['annee'];
+        $this->dateAjout = $data['date_ajout'];
+        $this->genre = '';
+        $this->publicVise = '';
+        $stmt->closeCursor();
+        $sql = "SELECT * FROM episode WHERE serie_id = ?";
+        $stmt = $conn->prepare($sql);
+        $stmt->execute([$id]);
+        $this->episodes = [];
+        $count = 0;
+        while($data = $stmt->fetch()){
+            $e = new Episode($data['id'],$data['numero']);
+            $this->episodes[] = $e;
+            $count++;
         }
+        $this->nbEpisodes = $count;
         $stmt->closeCursor();
     }
 
