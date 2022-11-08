d82443a02eb6dc4bd699a10ab6f3422b841fe4d6
adrien99920 <adrien.scher@gmail.com>
2022-11-08 09:11:54 +0100

    Ajoute la fonctionnalité d'affiche détaillé d'un episode d'une serie

--- src/classes/action/DisplaySerieEpisodeAction.php
+++ src/classes/action/DisplaySerieEpisodeAction.php
@@ -2,29 +2,24 @@
 
 namespace Application\action;
 
-use Application\datalayer\factory\ConnectionFactory;
+use Application\render\EpisodeRenderer;
+use Application\video\Episode;
 
 class DisplaySerieEpisodeAction extends Action{
 
     public function execute(): string{
        // get l'id de la seerie et l'id de l'episode
-        $_GET['episodeId'];
         if(!isset($_GET['episodeId']) && !isset($_GET['serieId'])){
             $html = "<p>Erreur lors de l'affichage</p>";
         }else{
             $serieId = $_GET['serieId'];
             $episodeId = $_GET['episodeId'];
 
-            $db = ConnectionFactory::getConnection();
-            $query = "select titre, resume, duree from episode where id = ? and serie_id = ?";
-            $stmt = $db->prepare($query);
-            $stmt->execute([$episodeId, $serieId]);
+            $episode = new Episode($serieId+0, $episodeId+0);
+            $renderer = new EpisodeRenderer($episode);
 
-            $row = $stmt->fetch();
+            $html = $renderer->render();
 
-            $html = "<p>Titre de l'epiosde : {$row['titre']}</p>";
-            $html .= "<p>Resumé de l'épiosde : {$row['resume']}</p>";
-            $html .= "<p>Durée de l'épiosde : {$row['duree']}</p>";
         }
         $html .= "<a href='index.php'>Retour page principale</a>";
         return $html;
