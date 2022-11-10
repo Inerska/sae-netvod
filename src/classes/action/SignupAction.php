533e4a5813d7850b5b926895042db07d37688c40
Alexis Chân Gridel <alexis.gridel@pm.me>
2022-11-10 11:37:01 +0100

    style(signin/up): total review with more importance, more indentation and detail stylized

--- src/classes/action/SignupAction.php
+++ src/classes/action/SignupAction.php
@@ -12,12 +12,18 @@ class SignupAction extends Action
     {
         if ($this->httpMethod === 'GET') {
             return <<<END
-                <form method="POST">
-                    <input type="email" name="email" poncopy="return false" onpaste="return false" placeholder="Adresse mail" required>
-                    <input type="password" name="password" oncopy="return false" onpaste="return false" placeholder="Password" required>
-                    <input type="password" name="confirm" oncopy="return false" onpaste="return false" placeholder="Confirm" required>
-                    <input type="submit" value="S'enregistrer">
-                </form>
+            <form method="post" class="flex justify-center items-center flex-col h-screen pb-72">
+                <h1 class="text-dark text-4xl font-light pb-5 dark:text-white">S'enregistrer</h1>
+                <div class="bg-gray-50 dark:bg-gray-700 p-10 w-1/2 flex items-center justify-center flex-col">
+                    <div class="flex flex-col gap-3 w-full">
+                            <input class="flex-1 w-full p-2 focus:outline-none bg-transparent border-b-2 dark:text-gray-100 placeholder-gray-200 focus:border-red-600 transition duration-300" type="email" name="email" oncopy="return false" onpaste="return false" placeholder="Email">
+                            <input class="flex-1 w-full p-2 focus:outline-none bg-transparent border-b-2 dark:text-gray-100 placeholder-gray-200 focus:border-red-600 transition duration-300" type="password" name="password" oncopy="return false" onpaste="return false" placeholder="Mot de passe">
+                            <input class="flex-1 w-full p-2 focus:outline-none bg-transparent border-b-2 dark:text-gray-100 placeholder-gray-200 focus:border-red-600 transition duration-300" type="password" name="confirm" oncopy="return false" onpaste="return false" placeholder="Confirmer votre mot de passe">
+                    </div>
+                </div>
+                <button type="submit" class="transition duration-300 bg-red-700 p-3 text-gray-800 font-semibold hover:bg-red-600 mt-5 uppercase">S'enregistrer</button>
+                <span class="dark:text-white pt-5">Vous avez déjà un compte ? <a href="?action=sign-in" class="text-red-700 font-semibold uppercase">Connectez-vous</a></span>
+            </form>
             END;
         }
 
