690e6de63c204b5862fdefdff1e74bd14ac199e1
Alexis Chân Gridel <alexis.gridel@pm.me>
2022-11-10 11:17:47 +0100

    style(signin): smaller mot de passe oublié

--- src/classes/action/SigninAction.php
+++ src/classes/action/SigninAction.php
@@ -14,21 +14,23 @@ class SigninAction extends Action
     {
 
         $html = <<<END
-        <form method="post" action="?action=sign-in" class="h-full flex items-center justify-center flex-col">
-            <h1 class="text-gray-900 dark:text-white text-4xl pb-5">Se connecter</h1>
-            <div class="bg-gray-100 dark:bg-gray-800 p-5">
-                <div class="flex flex-col gap-3">
-                    <input class="p-2 dark:bg-gray-800 bg-gray-100 border-b-4 mb-5 dark:text-gray-100" type="email" name="email" oncopy="return false" onpaste="return false" placeholder="votre email">
-                    <input class="p-2 dark:bg-gray-800 bg-gray-100 border-b-4 dark:text-gray-100" type="password" name="password" oncopy="return false" onpaste="return false" placeholder="votre mdp">
+        <div class="flex justify-center items-center h-screen pb-72">
+            <form method="post" action="?action=sign-in" class="bg-gray-50 dark:bg-gray-700 p-10 flex items-center justify-center flex-col">
+                <div class="">
+                    <h1 class="text-dark text-4xl font-sans font-semibold pb-5 dark:text-white">Se connecter</h1>
+                    <div class="flex flex-col gap-3">
+                        <input class="p-2 dark:bg-gray-800 bg-gray-100 border-b-4 mb-5 dark:text-gray-100" type="email" name="email" oncopy="return false" onpaste="return false" placeholder="votre email">
+                        <input class="p-2 dark:bg-gray-800 bg-gray-100 border-b-4 dark:text-gray-100" type="password" name="password" oncopy="return false" onpaste="return false" placeholder="votre mdp">
+                    </div>
+                    <a href="?action=renew" class="text-gray-900 dark:text-white font-sm text-xs">Mot de passe oublié ?</a>
                 </div>
-                <a href="?action=renew" class="text-gray-900 dark:text-white">Mot de passe oublié ?</a>
-            </div>
-            <button type="submit" class="bg-blue-500 p-3 text-white hover:bg-blue-600 mt-5">Connexion</button>
-        </form>
+                <button type="submit" class="bg-blue-500 p-3 text-white hover:bg-blue-600 mt-5">Connexion</button>
+            </form>
+        </div>
+        
         END;
 
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-
             $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
             $password = filter_var($_POST['password'], FILTER_SANITIZE_SPECIAL_CHARS);
 
@@ -37,7 +39,7 @@ class SigninAction extends Action
 
                 $db = ConnectionFactory::getConnection();
 
-                $query = $db->prepare("SELECT * FROM user WHERE email = ?");
+                $query = $db->prepare('SELECT * FROM user WHERE email = ?');
                 $query->execute([$email]);
                 $result = $query->fetch();
 
@@ -48,20 +50,16 @@ class SigninAction extends Action
                     $html .= "<p class='text-gray-900 dark:text-white'>Bienvenue $email</p>";
                     $_SESSION['loggedUser'] = serialize($user);
 
-                    header("Location: index.php");
+                    header('Location: index.php');
                     exit();
                 }
-
-
             } catch (AuthenticationException $e) {
                 $html .= "<p class='text-gray-900 dark:text-white'>Compte inexistant</p>";
             } catch (BadPasswordException $e) {
                 $html .= "<p class='text-gray-900 dark:text-white'>Mot de passe incorrect</p>";
             }
-
         }
 
         return $html;
-
     }
 }
