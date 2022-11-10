1914059f6889c85f9bc86504d809d75b179c7ba5
Alexis Chân Gridel <alexis.gridel@pm.me>
2022-11-10 17:01:57 +0100

    refactor: duplicate html code

--- src/classes/dispatch/Dispatcher.php
+++ src/classes/dispatch/Dispatcher.php
@@ -126,41 +126,18 @@ class Dispatcher
                 break;
         }
 
-        $this->render($html);
+        $this->renderCore($html);
     }
 
     private function render_grip(string $template): void
     {
-        require_once 'src/views/header.php';
-        echo <<<END
-            <!doctype html>
-            <html lang="fr" class="transition-colors duration-300">
-            <head>
-                <meta charset="UTF-8">
-                <meta name="viewport"
-                      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-                <meta http-equiv="X-UA-Compatible" content="ie=edge">
-                <title>NetVOD</title>
-                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
-                      integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
-                      crossorigin="anonymous" referrerpolicy="no-referrer"/>
-                <script src="https://code.jquery.com/jquery-3.6.1.min.js"
-                        integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
-                <script src="js/app.js"></script>
-                <link rel="stylesheet" href="css/tailwind.css">
-            </head>
-            <body class="bg-white dark:bg-gray-800 h-full transition-colors duration-700 antialiased">
-            
-            <div class='container w-screen'>$template </div>
-                        
-            </body>
-            </html>
-            END;
-    }
+        $template = <<<END
+           <div class='container w-screen'>
+            $template 
+            </div>
+        END;
 
-    private function render_partial(string $template): void
-    {
-        echo $template;
+        $this->render($template);
     }
 
     private function render(string $template): void
@@ -185,11 +162,28 @@ class Dispatcher
                 <link rel="stylesheet" href="css/tailwind.css">
             </head>
             <body class="bg-white dark:bg-gray-800 h-full transition-colors duration-700 antialiased">
-            
-            <div class='container mx-auto pt-4 w-screen'> $template </div>
-                        
+                $template 
             </body>
             </html>
             END;
+    }
+
+    private function render_partial(string $template): void
+    {
+        echo $template;
+    }
+
+    private function renderCore(string $template): void
+    {
+        $template = <<<END
+            <div class='container mx-auto pt-4 w-screen'> 
+                $template 
+            </div>
+                        
+            </body>
+            </html>
+        END;
+
+        $this->render($template);
     }
 }
