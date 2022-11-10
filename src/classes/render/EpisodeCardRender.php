36a9c2609dd4e65f21b1e928905b641b11f6ee37
adrien99920 <adrien.scher@gmail.com>
2022-11-10 17:03:31 +0100

    Css display serie

--- src/classes/render/EpisodeCardRender.php
+++ src/classes/render/EpisodeCardRender.php
@@ -15,7 +15,7 @@ class EpisodeCardRender implements Renderer {
 
     public function render(): string{
         $html = <<<END
-                        <a class="flex flex-col items-center w-2/6 bg-blue-100" href="index.php?action=display-series-episode&numEp={$this->episode->numero}&serieId={$this->episode->serieId}">
+                        <a class="flex flex-col bg-gray-100 items-center w-2/6 dark:bg-gray-700" href="index.php?action=display-series-episode&numEp={$this->episode->numero}&serieId={$this->episode->serieId}">
                                 <h1 class="text-red-600 text-2xl font-bold'">Episode {$this->episode->numero} - {$this->episode->titre}</h1>
                                 <video  class="h-58 w-40"><source src='video/{$this->episode->file}' type='video/mp4'></video>
                                 <p>Durée : {$this->episode->duree} secondes</p>
