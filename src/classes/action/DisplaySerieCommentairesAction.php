9de5050d4e0e12817b5b9c34c4b88ca8eb0240c7
POVOAS Florian <florian_povoas@icloud.com>
2022-11-10 19:16:15 +0100

    Better security

--- src/classes/action/DisplaySerieCommentairesAction.php
+++ src/classes/action/DisplaySerieCommentairesAction.php
@@ -9,31 +9,49 @@ class DisplaySerieCommentairesAction extends Action
 
     public function execute(): string
     {
+        if (!isset($_SESSION['loggedUser'])) {
+            header('Location: index.php');
+            exit();
+        }
 
         if(isset($_GET['serieId'])) {
 
 
             $serieId = $_GET['serieId'];
             $serie = new Serie($serieId);
-            $html = "<div class='commentaires'>" .
-                "<img class='h-58 w-40' src='{$serie->image}' alt='image de la série' /><br>" .
-                "<div id='list-commentaires'><h1>Commentaires - {$serie->titre}</h1>" .
-                "<ul><br>";
-            foreach ($serie->commentaires as $commentaire) {
-                $html .= <<<HTML
+
+            if ($serie->id == 0) {
+                $html = <<<END
+                <div class="flex justify-center items-center flex-col h-screen pb-72">
+                    <div class="bg-gray-50 dark:bg-gray-700 p-10 w-1/2 flex items-center justify-center flex-col">
+                        <h1 class="text-dark text-4xl font-light pb-5 dark:text-white">Serie introuvable</h1>
+                        <a href='index.php' class="text-gray-900 dark:text-white font-sm text-lg">Retour page principale</a>
+                    </div>
+                </div>
+                END;
+            } else {
+                $html = "<div class='commentaires'>" .
+                    "<img class='h-58 w-40' src='{$serie->image}' alt='image de la série' /><br>" .
+                    "<div id='list-commentaires'><h1>Commentaires - {$serie->titre}</h1>" .
+                    "<ul><br>";
+                foreach ($serie->commentaires as $commentaire) {
+                    $html .= <<<HTML
                 <li>
                     <p>Commentaire de {$commentaire['email']} : </p>
                     <p>&emsp;{$commentaire['commentaire']}</p>
                     <p>Note : {$commentaire['note']}/5</p>
                 </li><br>
             HTML;
-            }
-            if ($serie->nbCommentaires == 0) {
-                $html .= "<il>La série n'a pas encore été commentée.</il>";
-            }
-            $html .= "</ul></div>";
+                }
+                if ($serie->nbCommentaires == 0) {
+                    $html .= "<il>La série n'a pas encore été commentée.</il>";
+                }
+                $html .= "</ul></div>";
 
-            $html .= "<div id='retour-commentaire'><a href='?action=viewSerie&id={$serieId}'>Retour à la série</a></div></div>";
+                $html .= "<div id='retour-commentaire'><a href='?action=viewSerie&id={$serieId}'>Retour à la série</a></div></div>";
+            }
+
+
         }else{
             $html = "Erreur lors du chargement des commentaire";
         }
