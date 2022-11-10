9e6072f218386f951f5450dcfaceecd8607a6515
POVOAS Florian <florian_povoas@icloud.com>
2022-11-10 15:40:41 +0100

    Better css

--- src/classes/action/RenewAction.php
+++ src/classes/action/RenewAction.php
@@ -15,16 +15,28 @@ class RenewAction extends Action
         $html = "";
 
         if (isset($_SESSION['loggedUser'])) {
-            $html .= "<p>Vous êtes déjà connecté</p>";
+            $html .= <<<END
+                        <div class="flex justify-center items-center flex-col h-screen pb-72">
+                            <div class="bg-gray-50 dark:bg-gray-700 p-10 w-1/2 flex items-center justify-center flex-col">
+                                <h1 class="text-dark text-4xl font-light pb-5 dark:text-white">Vous êtes déjà connecté</h1>
+                            </div>
+                        </div>
+                        END;
         } else {
 
             if (!isset($_GET['token']) && !isset($_POST['token'])) {
                 if ($this->httpMethod === 'GET') {
 
                     $html .= <<<END
-                    <form method="POST">
-                        <input type="email" name="email" placeholder="Adresse mail" required>
-                        <input type="submit" value="Renouveler">
+                    <form method="POST" class="flex justify-center items-center flex-col h-screen pb-72">
+                        <h1 class="text-dark text-4xl font-light pb-5 dark:text-white">Renouveler le mot de passe</h1>
+                        <div class="bg-gray-50 dark:bg-gray-700 p-10 w-1/2 flex items-center justify-center flex-col">
+                            <div class="flex flex-col gap-3 w-full">
+                                <input class="flex-1 w-full p-2 focus:outline-none bg-transparent border-b-2 mb-5 dark:text-gray-100 placeholder-gray-200 focus:border-red-600 transition duration-300" type="email" name="email" placeholder="Adresse mail" required>
+                            </div>
+                        </div>
+                        <button type="submit" value="Renouveler" class="transition duration-300 bg-red-700 p-3 font-semibold text-white dark:text-gray-800 hover:bg-red-600 mt-5 uppercase">Renouveler</button>
+
                     </form>
                     END;
 
@@ -38,11 +50,21 @@ class RenewAction extends Action
                     if($query->fetch()) {
                         $token = AuthenticationIdentityService::generateRenewToken($email);
                         $html .= <<<END
-                        <p>Un email de renouvellement a été envoyé à l'adresse $email</p>
-                        <a href="index.php?action=renew&token={$token}">Changer le mot de passe</a>
+                        <div class="flex justify-center items-center flex-col h-screen pb-72">
+                            <div class="bg-gray-50 dark:bg-gray-700 p-10 w-1/2 flex items-center justify-center flex-col">
+                                <h1 class="text-dark text-4xl font-light pb-5 dark:text-white">Un email de renouvellement a été envoyé à l'adresse $email</h1>
+                                <a href="index.php?action=renew&token={$token}" class="text-gray-900 dark:text-white font-sm text-lg">Changer le mot de passe</a>
+                            </div>
+                        </div>
                         END;
                     } else {
-                        $html .= "<p>Cette adresse mail n'existe pas</p>";
+                        $html .= <<<END
+                        <div class="flex justify-center items-center flex-col h-screen pb-72">
+                            <div class="bg-gray-50 dark:bg-gray-700 p-10 w-1/2 flex items-center justify-center flex-col">
+                                <h1 class="text-dark text-4xl font-light pb-5 dark:text-white">Cette adresse mail n'existe pas</h1>
+                            </div>
+                        </div>
+                        END;
                     }
 
                 }
@@ -56,22 +78,39 @@ class RenewAction extends Action
                     if($result = $query->fetch()) {
                         if ($result['renewExpiration'] > time()) {
                             $html .= <<<END
-                            <form method="POST">
-                                <input type="hidden" name="token" value="$token">
-                                <input type="password" name="password" placeholder="Nouveau mot de passe" required>
-                                <input type="password" name="passwordConfirm" placeholder="Confirmation du mot de passe" required>
-                                <input type="submit" value="Changer le mot de passe">
+                            <form method="POST" class="flex justify-center items-center flex-col h-screen pb-72">
+                                <h1 class="text-dark text-4xl font-light pb-5 dark:text-white">Se connecter</h1>
+                                <div class="bg-gray-50 dark:bg-gray-700 p-10 w-1/2 flex items-center justify-center flex-col">
+                                    <div class="flex flex-col gap-3 w-full">
+                                        <input type="hidden" name="token" value="$token">
+                                        <input class="flex-1 w-full p-2 focus:outline-none bg-transparent border-b-2 mb-5 dark:text-gray-100 placeholder-gray-200 focus:border-red-600 transition duration-300" type="password" name="password" placeholder="Nouveau mot de passe" required>
+                                        <input class="flex-1 w-full p-2 focus:outline-none bg-transparent border-b-2 dark:text-gray-100 placeholder-gray-200 focus:border-red-600 transition duration-300" type="password" name="passwordConfirm" placeholder="Confirmation du mot de passe" required>
+                                    </div>
+                                </div>
+                                <button type="submit" class="transition duration-300 bg-red-700 p-3 font-semibold text-white dark:text-gray-800 hover:bg-red-600 mt-5 uppercase" value="Changer le mot de passe">Changer le mot de passe</button>
                             </form>
                             END;
 
                         } else {
                             $token = AuthenticationIdentityService::generateRenewToken($result['email']);
-                            $html = "<p>Votre lien a expiré, un nouveau lien a été généré</p>";
-                            $html .= "<p><a href='index.php?action=renew&token={$token}'>Changer le mot de passe</a></p>";
+                            $html .= <<<END
+                            <div class="flex justify-center items-center flex-col h-screen pb-72">
+                                <div class="bg-gray-50 dark:bg-gray-700 p-10 w-1/2 flex items-center justify-center flex-col">
+                                    <h1 class="text-dark text-4xl font-light pb-5 dark:text-white">Votre lien a expiré, un nouveau lien a été généré</h1>
+                                    <a href="index.php?action=renew&token={$token}" class="text-gray-900 dark:text-white font-sm text-lg">Changer le mot de passe</a>
+                                </div>
+                            </div>
+                            END;
                         }
 
                     } else {
-                        $html = "<p>Ce token ne correspond a aucun compte</p>";
+                        $html .= <<<END
+                        <div class="flex justify-center items-center flex-col h-screen pb-72">
+                                <div class="bg-gray-50 dark:bg-gray-700 p-10 w-1/2 flex items-center justify-center flex-col">
+                                    <h1 class="text-dark text-4xl font-light pb-5 dark:text-white">Ce token ne correspond a aucun compte</h1>
+                                </div>
+                            </div>
+                        END;
                     }
                 } else if ($this->httpMethod === 'POST') {
                     $token = $_POST['token'];
@@ -91,22 +130,45 @@ class RenewAction extends Action
                                 $query = $db->prepare("UPDATE user SET renewToken = NULL, renewExpiration = NULL, passwrd = :password WHERE id = :id");
                                 $hash = password_hash($password, PASSWORD_DEFAULT, ['cost' => 12]);
                                 $query->execute([':id' => $result['id'], ':password' => $hash]);
-                                $html = "<p>Votre mot de passe a bien été changé</p>";
-                                $html .= "<a href='index.php?action=sign-in'>Se connecter</a>";
+                                $html .= <<<END
+                                <div class="flex justify-center items-center flex-col h-screen pb-72">
+                                    <div class="bg-gray-50 dark:bg-gray-700 p-10 w-1/2 flex items-center justify-center flex-col">
+                                        <h1 class="text-dark text-4xl font-light pb-5 dark:text-white">Votre mot de passe a bien été changé</h1>
+                                        <a href="index.php?action=sign-in" class="text-gray-900 dark:text-white font-sm text-lg">Se connecter</a>
+                                    </div>
+                                </div>
+                                END;
                             } else {
-                                $html = "<p>Votre mot de passe n'est pas assez fort</p>";
-                                $html .= "<a href='index.php?action=renew&token={$token}'>Changer le mot de passe</a>";
+                                $html .= <<<END
+                                <div class="flex justify-center items-center flex-col h-screen pb-72">
+                                    <div class="bg-gray-50 dark:bg-gray-700 p-10 w-1/2 flex items-center justify-center flex-col">
+                                        <h1 class="text-dark text-4xl font-light pb-5 dark:text-white">Votre mot de passe n'est pas assez fort</h1>
+                                        <a href="index.php?action=renew&token={$token}" class="text-gray-900 dark:text-white font-sm text-lg">Changer le mot de passe</a>
+                                    </div>
+                                </div>
+                                END;
                             }
 
                         } else {
-                            $html = "<p>Les mots de passe ne correspondent pas</p>";
-                            $html .= "<a href='index.php?action=renew&token={$token}'>Changer le mot de passe</a>";
-                        }
+                            $html .= <<<END
+                                <div class="flex justify-center items-center flex-col h-screen pb-72">
+                                    <div class="bg-gray-50 dark:bg-gray-700 p-10 w-1/2 flex items-center justify-center flex-col">
+                                        <h1 class="text-dark text-4xl font-light pb-5 dark:text-white">Les mots de passe ne correspondent pas</h1>
+                                        <a href="index.php?action=renew&token={$token}" class="text-gray-900 dark:text-white font-sm text-lg">Changer le mot de passe</a>
+                                    </div>
+                                </div>
+                                END;
+                           }
 
                     } else {
-                        $html = "<p>Ce token ne correspond a aucun compte</p>";
-                        $html .= "<a href='index.php?action=renew&token={$token}'>Changer le mot de passe</a>";
-                    }
+                        $html .= <<<END
+                                <div class="flex justify-center items-center flex-col h-screen pb-72">
+                                    <div class="bg-gray-50 dark:bg-gray-700 p-10 w-1/2 flex items-center justify-center flex-col">
+                                        <h1 class="text-dark text-4xl font-light pb-5 dark:text-white">Ce token ne correspond a aucun compte</h1>
+                                    </div>
+                                </div>
+                                END;
+                        }
                 }
             }
         }
