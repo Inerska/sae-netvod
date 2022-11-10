0d38bf6cae0aee84793b3a2add15c6010d0d62c0
POVOAS Florian <florian_povoas@icloud.com>
2022-11-10 14:56:48 +0100

    Fixed year in serie

--- src/classes/action/DisplaySerieEnCours.php
+++ src/classes/action/DisplaySerieEnCours.php
@@ -19,7 +19,7 @@ class DisplaySerieEnCours extends Action{
             $s = "";
             while($row = $stmt->fetch()){
                 $serie = new Serie($row['idSerie']+0);
-                $renderer = new SeriesCardRenderer($serie->image, $serie->titre, $serie->id);
+                $renderer = new SeriesCardRenderer($serie->image, $serie->titre, $serie->id, $serie->annee);
                 $s .= $renderer->render();
             }
 
